e4607016136804206311bac3cdbcfdc9a05f5169
LynxSaiko <redteamsectorid@gmail.com>
2025-12-07 04:41:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+import os
+import shutil
 import curses
 from file_manager import FileManager
+from config import Config
+
+for root, dirs, files in os.walk(os.path.dirname(__file__)):
+    if "__pycache__" in dirs:
+        shutil.rmtree(os.path.join(root, "__pycache__"))
 
 def main(stdscr):
     if curses.has_colors():
         curses.start_color()
-    manager = FileManager(stdscr)
+
+    config = Config()
+    manager = FileManager(stdscr, config)
     manager.run()
 
 if __name__ == "__main__":
